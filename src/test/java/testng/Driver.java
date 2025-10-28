508c5011747e7a0e0930225840dcddbaec5bdd78
AVRAM ROBERT <avram@MacBook-Air-AVRAM.local>
2025-10-28 22:05:14 +0200

    Update Driver.java to use unique Chrome user data directory

--- src/test/java/testng/Driver.java
+++ src/test/java/testng/Driver.java
@@ -2,11 +2,25 @@ package testng;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Driver {
     public static WebDriver getLocalDriver() {
-        WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
+        try {
+            WebDriverManager.chromedriver().setup();
+
+            ChromeOptions options = new ChromeOptions();
+
+            // Creează un director temporar unic pentru fiecare rulare
+            Path tempUserDataDir = Files.createTempDirectory("chrome-user-data");
+            options.addArguments("--user-data-dir=" + tempUserDataDir.toAbsolutePath().toString());
+
+            return new ChromeDriver(options);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create ChromeDriver", e);
+        }
     }
 }
